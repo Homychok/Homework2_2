200dfb07be794f7ceab6ace1c69e35b057bb20c7
Alexandra <alexandravolchihina@yandex.ru>
2022-12-25 22:29:06 +0300

    Домашняя работа сделана

--- src/homework2_2/task1/Main.java
+++ src/homework2_2/task1/Main.java
@@ -2,12 +2,14 @@ package homework2_2.task1;
 
 import homework2_2.transport.Car;
 
+import java.time.LocalDate;
+
 
 public class Main {
 
         public static void main(String[] args){
             Car car1 = new Car("Lada", "Granta", 1.7, "yellow", 2015, "Russia", "transmission", 2, "winter", "ASF345S199");
-            Car.Insurance insurance = new Car.Insurance(1, 1000,"456677");
+            Car.Insurance insurance = new Car.Insurance(300, 1000,"456677", LocalDate.of(2022,12,22));
 
             System.out.println(car1 + "\n"/* + car2 + "\n" + car3 +
                 "\n" + car4 + "\n" + car5 */);
--- src/homework2_2/transport/Car.java
+++ src/homework2_2/transport/Car.java
@@ -1,6 +1,5 @@
 package homework2_2.transport;
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
 public class Car {
@@ -48,8 +47,9 @@ public class Car {
         protected Integer termOfTheInsurance;
         protected Integer costOfInsurance;
         protected String insuranceNumber;
+        protected LocalDate validityDate; // дата окончания страховки
 
-        public Insurance(Integer termOfTheInsurance, Integer costOfInsurance, String insuranceNumber) {
+        public Insurance(Integer termOfTheInsurance, Integer costOfInsurance, String insuranceNumber, LocalDate validityDate) {
             if (termOfTheInsurance == null) {
                 System.out.println("Некорректно введены данные");
             } else {
@@ -65,7 +65,17 @@ public class Car {
             } else {
                 this.insuranceNumber = null;
             }
+            this.validityDate = validityDate;
                    }
+
+        public LocalDate getValidityDate() {
+            return validityDate;
+        }
+
+        public void setValidityDate(LocalDate validityDate) {
+            this.validityDate = validityDate;
+        }
+
         public Integer getTermOfTheInsurance() {
             return termOfTheInsurance;
         }
@@ -78,6 +88,21 @@ public class Car {
 
             return insuranceNumber;
         }
+        public void periodOfInsurance() {
+
+//            LocalDate nowadays = LocalDate.of(2022, 12, 20);
+//            LocalDate dateOfBuyingIns = LocalDate.of(2022, 4, 13);
+//            Period daysOfInsurance1 = dateOfBuyingIns.until(nowadays);
+//            Long daysOfInsurance2 = dateOfBuyingIns.until(nowadays, ChronoUnit.DAYS);
+//            Long termOfTheInsurance = Long.valueOf(365);
+//            Integer lessDays = (int) (termOfTheInsurance - daysOfInsurance2);
+            if (validityDate.isAfter(LocalDate.now())) {
+                System.out.println("Осталось " + (LocalDate.now().until(validityDate, ChronoUnit.DAYS)) + " дней страховки.");
+            } else {
+                System.out.println("Страховка закончилась. Необходимо оформить новую!");
+            }
+        //    System.out.println("Прошло " + daysOfInsurance1 + " или " + daysOfInsurance2 + " дней со дня оформления страховки.");
+        }
             @Override
             public String toString () {
                 return "Длительность страховки "
@@ -85,20 +110,7 @@ public class Car {
                         ", стоимость страховки составила " + costOfInsurance +
                         " рублей, номер страховки " + insuranceNumber + ".";
             }
-        public void periodOfInsurance() {
-            LocalDate nowadays = LocalDate.of(2022, 12, 20);
-            LocalDate dateOfBuyingIns = LocalDate.of(2022, 4, 13);
-            Period daysOfInsurance1 = dateOfBuyingIns.until(nowadays);
-            Long daysOfInsurance2 = dateOfBuyingIns.until(nowadays, ChronoUnit.DAYS);
-            Long termOfTheInsurance = Long.valueOf(365);
-            Integer lessDays = (int) (termOfTheInsurance - daysOfInsurance2);
-            if (termOfTheInsurance > daysOfInsurance2) {
-                System.out.println("Осталось " + lessDays + " дней страховки.");
-            } else {
-                System.out.println("Страховка закончилась. Необходимо оформить новую!");
-            }
-            System.out.println("Прошло " + daysOfInsurance1 + " или " + daysOfInsurance2 + " дней со дня оформления страховки.");
-        }
+
         }
     public Car(String brand, String model, Double engineVolume,
                String color, Integer year, String country, String transmission, Integer numberOfSeats,
@@ -187,9 +199,9 @@ public void seasoneTireTypes(Integer monthNumber, Integer localTemperature, Stri
 //    String tireType = "summer";
     boolean seasonWinter;
     //    определяем принадлежность месяца к зимней резине
-    if (localTemperature <= 5 | monthNumber <= 3 | localTemperature <= 5 & monthNumber >= 10 & monthNumber <= 12) {
+    if (localTemperature <= 5 || monthNumber <= 3 || localTemperature <= 5 && monthNumber >= 10 && monthNumber <= 12) {
         seasonWinter = true;} else {seasonWinter = false;}
-    if (seasonWinter == true & tireType != "winter" | seasonWinter == false & tireType == "winter") {
+    if (seasonWinter == true && tireType != "winter" || seasonWinter == false && tireType == "winter") {
         System.out.println("Необходимо подобрать шины под сезон");} else {System.out.println("Шины подобраны под сезон");}
 
 }
